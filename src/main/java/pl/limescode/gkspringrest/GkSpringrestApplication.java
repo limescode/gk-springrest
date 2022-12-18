7571f912d16ec9337f5db95c772c02beda664084
limescode <katechizis@gmail.com>
2022-12-18 21:52:41 +0100

    initial commit
    initial commit

---
+++ src/main/java/pl/limescode/gkspringrest/GkSpringrestApplication.java
@@ -0,0 +1,13 @@
+package pl.limescode.gkspringrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GkSpringrestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GkSpringrestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/limescode/gkspringrest/GkSpringrestApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.limescode.gkspringrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GkSpringrestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
